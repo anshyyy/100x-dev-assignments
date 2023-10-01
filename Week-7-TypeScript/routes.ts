fc550be8eb465f37e5746d51e98eb07456c7efa9
Anshuman Sharma <anshuman9998@gmail.com>
2023-10-01 17:33:43 +0530

    typescript tutorial

--- Week-7-TypeScript/routes.ts
+++ Week-7-TypeScript/routes.ts
@@ -2,6 +2,18 @@ import express from 'express';
 import fs from 'fs';
 
 const router = express.Router();
+let dir : string = "D:/Dev/Harikirat Full Stack/Week-7-TypeScript/samples/";
+
+async function readFromFile(filename :string) : Promise<string> {
+    const fileName:string = filename;
+    return new Promise((res,rej)=>{
+        fs.readFile(dir+fileName,'utf8',(err,data)=>{
+            if(err) rej(err);
+            res(data);
+         });
+    })
+}
+
 
 router.get("/hi",(req,res)=>{
     res.status(200).json({
@@ -12,7 +24,7 @@ router.get("/hi",(req,res)=>{
 router.get("/files",(req,res)=>{
      try {
         console.log("hello ");
-        let dir : string = "D:/Dev/Harikirat Full Stack/Week-7-TypeScript/samples/";
+        
         let fileNames: string[]= fs.readdirSync(dir,'utf-8');
         res.status(200).json({
             message : "ok bro",
@@ -27,11 +39,18 @@ router.get("/files",(req,res)=>{
      }
 });
 
-router.get("file/:filename",(req,res)=>{
-    const fileName : string = req.params.filename;
+router.get("/file/:filename",async(req,res)=>{
+    
     try {
+        const fileName : string = req.params.filename;
+        const filedata = await readFromFile(fileName); 
         
+        return res.status(200).json({
+            data : filedata,
+            message : "ok",
+        })
     } catch (error) {
+        console.log(error);
         res.status(501).json({
             message : "SomeError has occured",
             err : error
